f6299e561400a2408436770f321460e6490e5379
Mazze LeCzzare <225266999+mazze93@users.noreply.github.com>
2025-11-08 14:31:41 -0500

    Add Powerlevel10k optional theme config.

---
+++ powerlevel10k.zsh
@@ -0,0 +1,6 @@
+# Example: Powerlevel10k theme config for Zsh
+# To use: source in .zshrc or use in Powerlevel10k prompt setup
+
+POWERLEVEL9K_MODE='nerdfont-complete'
+
+# ...rest of your theme settings here
